a04c87f06c4383fe4b8f3130f9c486351ec3defa
Oleh Mekhedok <oleh@voodoo.io>
2020-01-30 17:05:56 +0100

    added creator

---
+++ Creator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Zenject;
+
+public interface ICreator
+{
+    T Instantiate<T>(T original, Transform parent) where T : Object;
+    GameObject Instantiate(GameObject original, Transform parent);
+    T New<T>(params object[] extraArgs);
+}
+
+public class Creator : ICreator
+{
+    [Inject] private DiContainer _container = default;
+
+    public T Instantiate<T>(T original, Transform parent) where T : Object
+    {
+        return _container.InstantiatePrefabForComponent<T>(original, parent);
+    }
+
+    public GameObject Instantiate(GameObject original, Transform parent)
+    {
+        return _container.InstantiatePrefab(original, parent);
+    }
+
+    public T New<T>(params object[] extraArgs)
+    {
+        if (extraArgs == null)
+            extraArgs = new object[0];
+
+        var instance = _container.Instantiate<T>(extraArgs);
+
+        var initializable = instance as IInitializable;
+        initializable?.Initialize();
+
+        return instance;
+    }
+}
