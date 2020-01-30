28ff99d73926015c72dc8f569fed05a244a88e85
Oleh Mekhedok <oleh@voodoo.io>
2020-01-30 14:26:58 +0100

    added ReactiveCollection

---
+++ UniRxReactiveCollection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+
+public delegate void AddCallBack<in T>(T value);
+public delegate void RemoveCallBack<in T>(T value);
+
+public interface IObservableCollection<out T>: IEnumerable<T>
+{
+    IDisposable ObserveAdd(AddCallBack<T> callBack);
+    IObservable<int> ObserveCountChanged(bool notifyCurrentCount = false);
+    IDisposable ObserveRemove(RemoveCallBack<T> callBack);
+    IObservable<Unit> ObserveReset();
+}
+
+public class ReactiveCollection<T, TI> : ReactiveCollection<T>, IObservableCollection<TI> where T : TI
+{
+    IEnumerator<TI> IEnumerable<TI>.GetEnumerator()
+    {
+        foreach (var item in Items)
+        {
+            yield return item;
+        }
+    }
+
+    public IEnumerable<T> AsEnumerable()
+    {
+        foreach (var item in Items)
+        {
+            yield return item;
+        }
+    }
+
+    IDisposable IObservableCollection<TI>.ObserveRemove(RemoveCallBack<TI> callBack)
+    {
+        return ObserveRemove().Subscribe(s => callBack?.Invoke(s.Value));
+    }
+
+    IDisposable IObservableCollection<TI>.ObserveAdd(AddCallBack<TI> callBack)
+    {
+        return ObserveAdd().Subscribe(s => callBack?.Invoke(s.Value));
+    }
+
+    public void RemoveAll()
+    {
+        for (var i = Items.Count - 1; i >= 0; i--)
+        {
+            RemoveItem(i);
+        }
+    }
+}
